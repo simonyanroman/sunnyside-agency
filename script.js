34a5063a3273018bd3b9fba477071ad895415474
simonyanroman <simonyanrr@icloud.com>
2023-05-14 21:14:18 +0300

    qwerty

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".menu");
 
+let contactButton = document.querySelector(".contact");
+
+let isMenuOpen = false;
+
 hamburger.addEventListener("click", function () {
   menu.classList.toggle("hidden");
 });
