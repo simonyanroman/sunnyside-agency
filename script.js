f0f1bc14d7f0cc6a0318f80d11566ea025394d2c
simonyanroman <simonyanrr@icloud.com>
2023-05-17 20:18:33 +0300

    final-version

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let hamburger = document.querySelector(".hamburger");
-let menu = document.querySelector(".menu");
+let mobile_menu = document.querySelector(".mobile-menu");
 
 let contactButton = document.querySelector(".contact");
 
 let isMenuOpen = false;
 
 hamburger.addEventListener("click", function () {
-  menu.classList.toggle("hidden");
+  mobile_menu.classList.toggle("hidden");
 });
